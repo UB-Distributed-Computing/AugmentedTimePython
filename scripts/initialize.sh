c9f16c8dfbdc232a5b93d7ec66f1c862b25ae317
Bharadwaj Avva <bavva@timberlake.cse.buffalo.edu>
2014-04-03 09:32:07 -0400

    added scripts to initialize aws meaning install all needed softwares; compile and start driver in all of them; kill driver in all of them; collect logs from all of them

---
+++ scripts/initialize.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cat data.txt |awk '{print $1}' > ips.txt
+
+# for all ips in ips.txt file
+count=1
+for myip in `cat ips.txt`
+do
+
+cert=`cat data.txt |grep $myip|awk '{print $2}'`
+ssh -t -t -i cert/$cert ubuntu@$myip < setup.sh
+
+count=$((count + 1))
+done
+
+rm ips.txt
