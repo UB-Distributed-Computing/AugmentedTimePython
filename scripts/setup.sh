1ff5135602ec04f3b629bacf5e562bebbb461faa
Bharadwaj Avva <bavva@timberlake.cse.buffalo.edu>
2014-04-13 20:04:56 -0400

    changed scripts to compile Socket code

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -17,24 +17,24 @@ set background=dark
 yes Y|sudo apt-get update
 yes Y|sudo apt-get install gcc g++ make autoconf automake git libtool
 
-sudo rm -rf zeromq-4.0.4.tar.gz zeromq-4.0.4
-wget http://download.zeromq.org/zeromq-4.0.4.tar.gz
-tar xvf zeromq-4.0.4.tar.gz
+#sudo rm -rf zeromq-4.0.4.tar.gz zeromq-4.0.4
+#wget http://download.zeromq.org/zeromq-4.0.4.tar.gz
+#tar xvf zeromq-4.0.4.tar.gz
 
-curpath=`pwd`
-cd zeromq-4.0.4
-sudo ./configure
-sudo make
-sudo make install
-cd $curpath
-sudo rm -rf zeromq-4.0.4.tar.gz zeromq-4.0.4
+#curpath=`pwd`
+#cd zeromq-4.0.4
+#sudo ./configure
+#sudo make
+#sudo make install
+#cd $curpath
+#sudo rm -rf zeromq-4.0.4.tar.gz zeromq-4.0.4
 
 curpath=`pwd`
 rm -rf ~/AT
 mkdir -p ~/AT
 cd ~/AT
 git clone https://github.com/UB-Distributed-Computing/AugmentedTimePython.git
-cd AugmentedTimePython/AT-ZMQ
+cd AugmentedTimePython/Socket
 make
 cd $curpath
 exit
